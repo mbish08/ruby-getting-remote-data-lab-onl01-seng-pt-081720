a0f55348832b4fa6a7a4a1e8f0ec541702f3bbb6
Mikel Bishop <mdrew8@gmail.com>
2020-09-27 20:01:20 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -15,9 +15,9 @@ class GetRequester
   end 
   
   def parse_json 
-    programs = JSON.parse(self.get_response_body)
-  programs.collect do |program|
-    program["agency"] 
+    results = JSON.parse(self.get_response_body)
+    results.collect do |result|
+      result
   end 
 end
   
